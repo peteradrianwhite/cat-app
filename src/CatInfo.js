a6718fb488c22541370045f7c4ddbc038c1882ee
peteradrianwhite <peteradrianwhite@gmail.com>
2024-06-25 07:43:37 -0300

    Updated stylings and added favorite bar

--- src/CatInfo.js
+++ src/CatInfo.js
@@ -5,6 +5,7 @@ import './CatInfo.css';
 const CatInfo = () => {
   const [catImage, setCatImage] = useState('');
   const [CatInfo, setCatInfo] = useState('');
+  const [favorites, setFavorites] = useState([]);
 
   const fetchCatData = async () => {
     try {
@@ -20,12 +21,23 @@ const CatInfo = () => {
   const fetchCatGif = async () => {
     try {
          
-      setCatImage(`https://cataas.com/cat/gif?timestamp=${new Date().getTime()}` );
+      setCatImage(`https://cataas.com/cat/gif?` +  new Date().getTime());
       const factResponse = await axios.get('https://catfact.ninja/fact');
       setCatInfo(factResponse.data.fact);
     } catch (error) {
       console.error('Error fetching cat gif:', error);
     }
+  };
+
+
+  const saveCatPic = () => {
+    setFavorites(prevFavorites => {
+      const newFavorites = [...prevFavorites, catImage];
+      if (newFavorites.length > 4) {
+        newFavorites.shift(); 
+      }
+      return newFavorites;
+    });
   };
 
   useEffect(() => {
@@ -36,11 +48,22 @@ const CatInfo = () => {
     <div className="cat-fact-container">
       <img src={catImage} alt="Random Cat" className="cat-image" />
       <p className="cat-fact">{CatInfo}</p>
+      <div className="button-container">
       <button onClick={fetchCatData} className="refresh-button">Refresh</button> 
-      <br/>
-      <button onClick={fetchCatGif} className="gif-button">How about a gif?</button> <label>Gifs Take a Short Time to Load</label>
-      
+      <button onClick={fetchCatGif} className="gif-button">How about a gif?</button> 
+      <button onClick={saveCatPic} className="save-button">Save Cat Pic</button>
+      <div style={{height:'20px'}}>
+      <label style={{fontSize:'12pt'}}>Gifs Take a weirdly long time to to load from the API</label>
+      </div>
     </div>
+        <div className="favorites-container">
+            {favorites.map((fav, index) => (
+            <img key={index} src={fav} alt={`Favorite ${index + 1}`} className="favorite-image" />
+            ))}
+      </div>
+    </div>
+
+    
   );
 };
 
