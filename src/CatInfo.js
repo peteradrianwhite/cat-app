76e5067f6f5fcfa775b571b76701893f233551a6
peteradrianwhite <peteradrianwhite@gmail.com>
2024-06-25 08:07:26 -0300

    Removed gifs

--- src/CatInfo.js
+++ src/CatInfo.js
@@ -18,17 +18,6 @@ const CatInfo = () => {
     }
   };
 
-  const fetchCatGif = async () => {
-    try {
-         
-      setCatImage(`https://cataas.com/cat/gif?` +  new Date().getTime());
-      const factResponse = await axios.get('https://catfact.ninja/fact');
-      setCatInfo(factResponse.data.fact);
-    } catch (error) {
-      console.error('Error fetching cat gif:', error);
-    }
-  };
-
 
   const saveCatPic = () => {
     setFavorites(prevFavorites => {
@@ -50,11 +39,8 @@ const CatInfo = () => {
       <p className="cat-fact">{CatInfo}</p>
       <div className="button-container">
       <button onClick={fetchCatData} className="refresh-button">Refresh</button> 
-      <button onClick={fetchCatGif} className="gif-button">How about a gif?</button> 
       <button onClick={saveCatPic} className="save-button">Save Cat Pic</button>
-      <div style={{height:'20px'}}>
-      <label style={{fontSize:'12pt'}}>Gifs Take a weirdly long time to to load from the API and occassionally fail. But they're very cute.</label>
-      </div>
+      
     </div>
         <div className="favorites-container">
             {favorites.map((fav, index) => (
